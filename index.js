f48790f378a2f12c5cc81e9739ff0dac12ee5b6f
Othman AMD <red.fantastic@hotmail.com>
2017-09-25 12:01:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,13 +14,12 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 
+
 function sayHiToGrandma(string){
   if(string == string.toLowerCase()){
     return "I can\'t hear you!"
   }
-}
-
-function sayHiToGrandma(string){
+  
   if(string == string.toUpperCase()){
     return "YES INDEED!"
   }
